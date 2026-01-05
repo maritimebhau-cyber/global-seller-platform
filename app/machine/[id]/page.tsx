f00eb8b5607bde09db7595aba7cb200f1cece846
Sashakt <saskhatenterprises@gmail.com>
2026-01-06 03:48:41 +0530

    update the code

--- app/machine/[id]/page.tsx
+++ app/machine/[id]/page.tsx
@@ -1,6 +1,6 @@
-import MachineList from "../../component/MachineList/MachineList";
-import "./page.css";
-export default function Page() {
-  return <MachineList />;
-}
+// import MachineList from "../../component/MachineList/MachineList";
+// import "./page.css";
+// export default function Page() {
+//   return <MachineList />;
+// }
              
